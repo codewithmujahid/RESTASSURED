89380f9461302ca6175f285501906956e7c11023
codewithmujahid <codewithmujahid@gmail.com>
2026-06-13 23:37:57 +0530

    commit this changes in the main

--- src/test/java/RestAssured/RestAssured/complexjson.java
+++ src/test/java/RestAssured/RestAssured/complexjson.java
@@ -11,50 +11,50 @@ public class complexjson {
 	
 	public void json() {
 
-//		String request = """
-//		{
-//		  "user": {
-//		    "id": 101,
-//		    "name": "Mujahid",
-//		    "contact": {
-//		      "email": "mujahid@test.com",
-//		      "phone": "9876543210"
-//		    },
-//		    "address": {
-//		      "city": "Pune",
-//		      "state": "Maharashtra",
-//		      "pincode": "411001"
-//		    }
-//		  },
-//		  "order": {
-//		    "orderId": "ORD123",
-//		    "items": [
-//		      {
-//		        "productId": "P101",
-//		        "productName": "Mobile",
-//		        "price": 15000
-//		      },
-//		      {
-//		        "productId": "P102",
-//		        "productName": "Headphone",
-//		        "price": 2000
-//		      }
-//		    ]
-//		  }
-//		}
-//		""";
+		String request = """
+		{
+		  "user": {
+		    "id": 101,
+	    "name": "Mujahid",
+	    "contact": {
+	      "email": "mujahid@test.com",
+	      "phone": "9876543210"
+	    },
+	    "address": {
+	      "city": "Pune",
+	      "state": "Maharashtra",
+	      "pincode": "411001"
+	    }
+	  },
+	  "order": {
+	    "orderId": "ORD123",
+	    "items": [
+	      {
+	        "productId": "P101",
+	        "productName": "Mobile",
+	        "price": 15000
+	      },
+	      {
+	        "productId": "P102",
+	        "productName": "Headphone",
+	        "price": 2000
+	      }
+	    ]
+	  }
+	}
+	""";
 		
-//		 Response responce =
-//				 given()
-//				 .contentType(ContentType.JSON)
-//				 .body(request)
-//				 .when()
-//				 .post("https://reqres.in//api/users")
-//				 .then()
-//				 .responce;
-//				 
+	 Response responce =
+			 given()
+			 .contentType(ContentType.JSON)
+			 .body(request)
+			 .when()
+			 .post("https://reqres.in//api/users")
+			 .then()
+			 .responce;
+			 
 //
-//				 
+		 
 			       
 
 		
